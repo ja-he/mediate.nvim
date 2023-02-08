de93964400161a52903a10665127c417cd8ceb65
Jan Hensel <ja_he@uni-bremen.de>
2023-02-08 15:34:54 +0100

    Sanity-check mediation start

--- lua/mediate/init.lua
+++ lua/mediate/init.lua
@@ -11,9 +11,26 @@ local original_buf = -1
 local original_line_start = -1
 local original_line_end = -1
 
+local match_conflict_start = function(s)
+  local start_marker = "<<<<<<<"
+  local starts_with_marker = string.sub(s, 1, string.len(start_marker)) == start_marker
+  return starts_with_marker
+end
+
 local mediate_start = function()
+  -- ensure not already started
+  if mediation_active then
+    print("ILLEGAL: mediation already active")
+    do return end
+  end
+
   -- get text from cursor
+  local current_line_contents = vim.api.nvim_get_current_line()
   -- verify that at conflict (by marker)
+  if not match_conflict_start(current_line_contents) then
+    print("ILLEGAL: current line not a conflict start")
+    do return end
+  end
   -- split into party A and B (by markers)
 
   -- open new tab
